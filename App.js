f2c79cdad476ae25443ba0a2eb2020656f7db625
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2023-09-29 00:51:04 +0500

    create custom date picker component

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
+import React, { useState } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { Screen, AppTextInput } from './app/components';
+import { Screen, AppTextInput, AppPicker } from './app/components';
+
+const categories = [
+  { label: 'Furniture', value: 1 },
+  { label: 'Clothing', value: 2 },
+  { label: 'Camera', value: 3 },
+];
 
 export default function App() {
+  const [category, setCategory] = useState('');
+
   return (
     <GestureHandlerRootView>
       <Screen>
-        <AppTextInput placeholder="Username" icon="email" />
+        <AppPicker
+          icon="apps"
+          items={categories}
+          onSelectedItem={(item) => setCategory(item)}
+          placeholder="Category"
+          selectedItem={category}
+        />
+        <AppTextInput placeholder="Email" icon="email" />
       </Screen>
     </GestureHandlerRootView>
   );
--- app/components/index.js
+++ app/components/index.js
@@ -1,9 +1,11 @@
-export { default as AppButton } from './AppButton';
-export { default as Card } from './Card';
 export { default as AppText } from './AppText';
+export { default as AppPicker } from './AppPicker';
+export { default as AppButton } from './AppButton';
+export { default as AppTextInput } from './AppTextInput';
+export { default as Card } from './Card';
 export { default as ListItem } from './ListItem';
-export { default as Screen } from './Screen';
 export { default as ListItemSeparator } from './ListItemSeparator';
 export { default as ListItemDeleteAction } from './ListItemDeleteAction';
 export { default as Icon } from './Icon';
-export { default as AppTextInput } from './AppTextInput';
+export { default as Screen } from './Screen';
+export { default as PickerItem } from './PickerItem';
