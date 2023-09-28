0d9a846d1b3c1320444e09fc096c96f88b630086
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2023-09-29 02:27:29 +0500

    create login screen and reusable components

--- App.js
+++ App.js
@@ -1,28 +1,11 @@
 import React, { useState } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { Screen, AppTextInput, AppPicker } from './app/components';
-
-const categories = [
-  { label: 'Furniture', value: 1 },
-  { label: 'Clothing', value: 2 },
-  { label: 'Camera', value: 3 },
-];
+import LoginScreen from './app/screens/LoginScreen/LoginScreen';
 
 export default function App() {
-  const [category, setCategory] = useState('');
-
   return (
     <GestureHandlerRootView>
-      <Screen>
-        <AppPicker
-          icon="apps"
-          items={categories}
-          onSelectedItem={(item) => setCategory(item)}
-          placeholder="Category"
-          selectedItem={category}
-        />
-        <AppTextInput placeholder="Email" icon="email" />
-      </Screen>
+      <LoginScreen />
     </GestureHandlerRootView>
   );
 }
--- app/screens/index.js
+++ app/screens/index.js
@@ -1,6 +1,7 @@
-export { default as Welcome } from './Welcome/Welcome';
-export { default as ViewImage } from './ViewImage/ViewImage';
-export { default as ListingDetails } from './ListingDetails/ListingDetails';
-export { default as Messages } from './Messages/Messages';
 export { default as AccountScreen } from './AccountScreen/AccountScreen';
+export { default as ListingDetails } from './ListingDetails/ListingDetails';
 export { default as ListingScreen } from './ListingScreen/ListingScreen';
+export { default as LoginScreen } from './LoginScreen/LoginScreen';
+export { default as Messages } from './Messages/Messages';
+export { default as ViewImage } from './ViewImage/ViewImage';
+export { default as Welcome } from './Welcome/Welcome';
