ead9c8730d80a76ad0cce1b4ea85b69bb5840899
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2023-09-29 23:10:11 +0500

    improve the ListItem component

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import ListEditScreen from './app/screens/ListEditScreen/ListEditScreen';
+
+import { ListItem } from './app/components';
 
 export default function App() {
   return (
     <GestureHandlerRootView>
-      <ListEditScreen />
+      <SafeAreaView style={{ paddingVertical: 50 }}>
+        <ListItem
+          title="Mosh Hamedani"
+          subTitle="code with mosh"
+          image={require('./app/assets/mosh.jpg')}
+        />
+      </SafeAreaView>
     </GestureHandlerRootView>
   );
 }
