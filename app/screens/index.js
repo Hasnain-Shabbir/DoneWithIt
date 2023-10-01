e8711208e0bcfd1d66ca367eb4e960f38c8fe751
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2023-10-01 12:17:10 +0500

    create register screen

--- app/screens/index.js
+++ app/screens/index.js
@@ -3,6 +3,7 @@ export { default as ListingDetails } from './ListingDetails/ListingDetails';
 export { default as ListingScreen } from './ListingScreen/ListingScreen';
 export { default as LoginScreen } from './LoginScreen/LoginScreen';
 export { default as Messages } from './Messages/Messages';
+export { default as RegisterScreen } from './RegisterSceen/RegisterSceen';
 export { default as ViewImage } from './ViewImage/ViewImage';
 export { default as Welcome } from './Welcome/Welcome';
 export { default as ListEditScreen } from './ListEditScreen/ListEditScreen';
