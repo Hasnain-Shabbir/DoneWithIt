6c3a52c9bb4d294293642b876fb1eba86d6af35d
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2023-10-05 16:17:03 +0500

    update the listings screen

--- app/screens/ListingScreen/ListingScreen.jsx
+++ app/screens/ListingScreen/ListingScreen.jsx
@@ -1,13 +1,7 @@
 import React, { useEffect } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 
-import {
-  AppText,
-  ActivityIndicator,
-  Card,
-  ApiError,
-  Screen,
-} from '../../components';
+import { ActivityIndicator, Card, ApiError, Screen } from '../../components';
 import colors from '../../config/colors';
 import listingsApi from '../../api/listings';
 import routes from '../../navigation/routes';
@@ -26,33 +20,30 @@ const ListingScreen = ({ navigation }) => {
   }, []);
 
   return (
-    <Screen style={styles.screen}>
-      {error && (
-        <ApiError
-          errorMessage="Coudn't retrieve the listings."
-          title="Retry"
-          onPress={loadListings}
-        />
-      )}
-      {loading && (
-        <>
-          <ActivityIndicator visible={true} />
-          <AppText>Loading the data.</AppText>
-        </>
-      )}
-      <FlatList
-        data={listings}
-        keyExtractor={(listing) => listing.id.toString()}
-        renderItem={({ item }) => (
-          <Card
-            title={item.title}
-            subTitle={`$${item.price}`}
-            imageUrl={item.images[0].url}
-            onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
+    <>
+      <ActivityIndicator visible={loading} />
+      <Screen style={styles.screen}>
+        {error && (
+          <ApiError
+            errorMessage="Coudn't retrieve the listings."
+            title="Retry"
+            onPress={loadListings}
           />
         )}
-      />
-    </Screen>
+        <FlatList
+          data={listings}
+          keyExtractor={(listing) => listing.id.toString()}
+          renderItem={({ item }) => (
+            <Card
+              title={item.title}
+              subTitle={`$${item.price}`}
+              imageUrl={item.images[0].url}
+              onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
+            />
+          )}
+        />
+      </Screen>
+    </>
   );
 };
 
